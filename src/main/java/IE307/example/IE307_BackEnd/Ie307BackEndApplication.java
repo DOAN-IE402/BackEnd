acff4c4f14b214607f6d9db25394e7e52d8c7abb
hvupham <21522804@gm.uit.edu.vn>
2024-10-27 00:41:36 +0700

    init BE

---
+++ src/main/java/IE307/example/IE307_BackEnd/Ie307BackEndApplication.java
@@ -0,0 +1,13 @@
+package IE307.example.IE307_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ie307BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ie307BackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IE307/example/IE307_BackEnd/Ie307BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package IE307.example.IE307_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ie307BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
